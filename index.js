c3982a89f2c21f0acb9f6ca93e1f7f06411528ac
moshesh81 <moshe991@gmail.com>
2021-09-20 14:57:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $(".navbar").addClass("sticky");
+        }else{
+            $(".navbar").removeClass("sticky");
+        }
+
+    });
+    //slide-up script
+ 
+    //nav-bar toggle 
+    $(".menu-btn").click(function(){
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active")
+    });
+   var t= new Typed(".typing",{
+        strings:["FullStack","Developer", "FullStack", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed:60,
+        loop: true
+    });
+    //owl carousel script
+    $(".carousel").owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1,
+                nav: false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+           1000:{
+               items:3,
+               nav:false
+           }
+        }
+    });
+});
